afcadb324ccfdd6043a2178f96d24a7d72536f9c
Sipho Mancam <siphomancam2@gmail.com>
2024-12-20 10:55:29 +0200

    Added build config before test

--- App.py
+++ App.py
@@ -2,10 +2,13 @@ from view import CricketTrackingWidget, load_style_sheet
 from PyQt5.QtWidgets import QApplication
 import sys
 
-if __name__ == "__main__":
+
+def main():
     app = QApplication(sys.argv)
     app.setStyleSheet(load_style_sheet('./main_styles.qss'))
     cricket_view = CricketTrackingWidget(None)
     cricket_view.show()
-    
     sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    main()
